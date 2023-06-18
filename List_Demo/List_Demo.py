774740ea66be54e7c902cfb033d49696192ec38a
LittleSeven <kailzhang893@outlook.com>
2023-06-18 14:32:30 +0800

    list

---
+++ List_Demo/List_Demo.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+'''
+-----------------------------------------------------------
+    @FileName  :List_Demo.py
+    @Time      :2023/6/18 14:30
+    @Author    :LittleSeven
+    @Address   ：https://gitee.com/linguchong/Django_Project_Demo.git
+-----------------------------------------------------------
+'''
+
