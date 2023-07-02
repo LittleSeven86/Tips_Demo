b90518c1a04f12fabe742e3f0043e940bdc87dea
LittleSeven <kailzhang893@outlook.com>
2023-07-02 13:32:05 +0800

    add break

---
+++ Looping structures/while loop/while loop.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+'''
+-----------------------------------------------------------
+    @FileName  :while loop.py
+    @Time      :2023/7/2 12:09
+    @Author    :LittleSeven
+    @Address   ：https://gitee.com/linguchong/Django_Project_Demo.git
+-----------------------------------------------------------
+'''
+
+total = 0
+i = 1
+
+while i <= 5:
+    total += i
+    i += 1
+
+print("Sum:", total)
