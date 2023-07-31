9446b7093d7b095706a8fa955f8f23fdd9e12333
LittleSeven86 <kailzhang893@outlook.com>
2023-07-31 17:04:36 +0800

    更新内容

--- 继承/继承.py
+++ 继承/继承.py
@@ -6,20 +6,4 @@
  @DateTime: 2023/7/28 15:32
  @SoftWare: PyCharm
 """
-class Animal:
-    def make_sound(self):
-        print("这是动物的叫声")
-
-
-class Dog(Animal):
-    def make_sound(self):
-        print("汪汪汪~狗再叫！")
-
-
-animal = Animal()
-animal.make_sound()  # 输出: 这是动物的叫声
-
-dog = Dog()
-dog.make_sound()  # 输出: 汪汪汪~狗再叫！
-
 
