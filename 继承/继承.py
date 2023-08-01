194db5b2bcd1fee292793011cfa9ac58d2af7ab3
LittleSeven86 <kailzhang893@outlook.com>
2023-08-01 17:18:23 +0800

    更新内容

--- 继承/继承.py
+++ 继承/继承.py
@@ -7,3 +7,39 @@
  @SoftWare: PyCharm
 """
 
+class Father(object):
+    def name(self):
+        print('这是爸爸~')
+
+class Son(Father):
+    def name(self):
+        super().name()
+        print('这是儿子~')
+
+Son().name()
+# print(dir(object))
+
+class Demo:
+    def __init__(self, age):
+        self.age = age
+    
+    def __getattr__(self, item):
+        print(f'获取到了属性：{item}')
+        return self.item
+
+    def __setattr__(self, key, value):
+        print(f"设置属性: {key} 为 {value}")
+        super().__setattr__(key, value)
+
+    def __delattr__(self, item):
+        print(f'删除属性：{item}')
+        super().__delattr__(item)
+
+    # def __getattribute__(self, item):
+    #     print(f'获取到了属性{item}')
+    #     return self.age
+
+
+demo = Demo(20)
+# demo.age  # 输出: 获取到了属性age
+print(demo.name)
