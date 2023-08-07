6d8a1d3de3e33a629d22afdec635de9f0f760169
LittleSeven86 <kailzhang893@outlook.com>
2023-08-07 09:37:05 +0800

    更新多态

--- 多态/duotai.py
+++ 多态/duotai.py
@@ -33,9 +33,52 @@ class TestCase(Model):
     def update(self):
         print('更新用例基本信息')
 
+def get_info(obj):
+    obj.get()
+
 
 project1 = ProjectModels()
 project1.get()
 
 case1 = TestCase()
 case1.get()
+get_info(case1)
+get_info(project1)
+
+from abc import ABC, abstractmethod
+
+
+class Shape(ABC):
+    @abstractmethod
+    def calculate_area(self):
+        pass
+
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def calculate_area(self):
+        return self.length * self.width
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def calculate_area(self):
+        return 3.14 * self.radius * self.radius
+
+
+def print_area(shape):
+    print("The area is:", shape.calculate_area())
+
+
+# 创建不同类型的形状对象
+rectangle = Rectangle(5, 3)
+circle = Circle(4)
+
+# 使用统一的接口打印面积
+print_area(rectangle)  # 输出：The area is: 15
+print_area(circle)  # 输出：The area is: 50.24
